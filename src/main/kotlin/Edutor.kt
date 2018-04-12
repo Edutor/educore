5540bc3b9b412cccc759b0534b843d73a6e47bc8
Tine Marbjerg (TM - Lektor - Cphbusiness) <tm@cphbusiness.dk>
2018-04-12 14:53:13 +0200

    new Educor says

--- src/main/kotlin/Edutor.kt
+++ src/main/kotlin/Edutor.kt
@@ -2,7 +2,7 @@ import dk.edutor.eduport.Port
 import dk.edutor.eduport.jarchecker.JarChecker
 
 fun main(args: Array<String>) {
-  println("Edutor core ...")
+  println("Edutor core says...")
   val port = JarChecker()
   port.sayHello()
   }
