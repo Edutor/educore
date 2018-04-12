5612bd68c6b24a493af3210a90fc04d8c01ec15f
Christian Nielsen (CNLS - Adjunkt - Cphbusiness) <cnls@cphbusiness.dk>
2018-04-12 14:48:07 +0200

    NettyServer added in main

--- src/main/kotlin/Edutor.kt
+++ src/main/kotlin/Edutor.kt
@@ -1,8 +1,33 @@
 import dk.edutor.eduport.Port
 import dk.edutor.eduport.jarchecker.JarChecker
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.*
+import io.ktor.gson.gson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import java.text.DateFormat
+
 
 fun main(args: Array<String>) {
-  println("Edutor core ...")
-  val port = JarChecker()
-  port.sayHello()
-  }
+    val server = embeddedServer(Netty, 8080) {
+        install(DefaultHeaders)
+        install(Compression)
+        install(ContentNegotiation) {
+            gson {
+                setDateFormat(DateFormat.LONG)
+                setPrettyPrinting()
+            }
+        }
+        install(CallLogging)
+        routing {
+            get("/hello") {
+                call.respond("goodbye")
+            }
+        }
+    }
+    server.start(wait = true)
+}
