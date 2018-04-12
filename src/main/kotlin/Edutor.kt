7f08200b5e56bbee8c09c4df6bed5c09dda56841
Anders Kalhauge <anders@kalhauge.dk>
2018-04-12 17:24:14 +0200

    Added static content

--- src/main/kotlin/Edutor.kt
+++ src/main/kotlin/Edutor.kt
@@ -2,6 +2,7 @@ import dk.edutor.eduport.Port
 import dk.edutor.eduport.jarchecker.JarChecker
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.content.*
 import io.ktor.features.*
 import io.ktor.gson.gson
 import io.ktor.response.respond
@@ -15,18 +16,22 @@ fun main(args: Array<String>) {
     val server = embeddedServer(Netty, 8080) {
         install(DefaultHeaders)
         install(Compression)
+        install(CallLogging)
         install(ContentNegotiation) {
             gson {
                 setDateFormat(DateFormat.LONG)
                 setPrettyPrinting()
+                }
             }
-        }
-        install(CallLogging)
         routing {
             get("/hello") {
                 call.respond("goodbye")
+                }
+            static("") {
+                resources("www")
+                defaultResource("index.html", "www")
+                }
             }
         }
-    }
     server.start(wait = true)
-}
+    }
