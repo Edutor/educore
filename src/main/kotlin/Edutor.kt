8e20afc743b2c84e2621fa2b2245af0c0d6eed97
Thomas-Hartmann <thomas@webtrade.dk>
2018-04-30 18:16:04 +0200

    added post method

--- src/main/kotlin/Edutor.kt
+++ src/main/kotlin/Edutor.kt
@@ -1,10 +1,5 @@
 import com.google.gson.GsonBuilder
-import dk.edutor.eduport.Challenge
-import dk.edutor.eduport.MCChallenge
-import dk.edutor.eduport.StringChallenge
-import dk.edutor.eduport.StringSolution
-import dk.edutor.eduport.PersonIdentifier
-
+import dk.edutor.eduport.*
 import dk.edutor.eduport.jarchecker.JarChecker
 import dk.edutor.eduport.simple.SimpleChecker
 import io.ktor.application.call
@@ -12,17 +7,21 @@ import io.ktor.application.install
 import io.ktor.content.defaultResource
 import io.ktor.content.resources
 import io.ktor.content.static
-import io.ktor.features.CallLogging
-import io.ktor.features.Compression
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.DefaultHeaders
+import io.ktor.features.*
 import io.ktor.gson.gson
+import io.ktor.request.PartData
+import io.ktor.request.isMultipart
+import io.ktor.request.receiveMultipart
+import io.ktor.response.header
 import io.ktor.response.respond
+import io.ktor.response.respondWrite
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import java.text.DateFormat
+import java.time.Duration
 
 val gson = GsonBuilder().setPrettyPrinting().create()
 
@@ -33,8 +32,14 @@ val simpleChecker = SimpleChecker()
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, 8080)
     {
+        install(DefaultHeaders){
+//            header("access-Control-Allow-Origin", "*")
+//            header("access-control-allow-methods", "GET, HEAD")
 
-        install(DefaultHeaders)
+        }
+        install(CORS){
+            anyHost()
+        }
         install(Compression)
         install(CallLogging)
         install(ContentNegotiation) {
@@ -47,11 +52,43 @@ fun main(args: Array<String>) {
             get("/hello") {
                 call.respond("goodbye")
             }
+            get("/tag"){
+                call.respond(gson.toJson(allTags))
+            }
             get("/challenge/tag/{tagname}"){
                 val tagname = call.parameters["tagname"]?:"" //Should maybe change to !! (double bang) to get an exception when tagname is null?
                 call.respond(gson.toJson(getChallengeSet(listOf(tagname))))
             }
+            post("/postdemo/"){
+                val multipart = call.receiveMultipart()
+                call.respondWrite {
+                    if (!call.request.isMultipart()) {
+                        appendln("Not a multipart request")
+                    } else {
+                        while (true) {
+                            val part = multipart.readPart() ?: break
 
+                            when (part) {
+                                is PartData.FormItem -> {
+                                    val value = part.value
+                                    println(value)//
+
+                                    //VIRKER IKKE ENDNU
+                                    val solution = gson.fromJson(value, MCSolution::class.java)
+                                    println(solution)
+//                                    appendln("Form field: ${part.partName} = ${part.value}")
+//                                    val solution = MCSolution()
+                                }
+                                is PartData.FileItem -> appendln("File field: ${part.partName} -> ${part.originalFileName} of ${part.contentType}")
+                            }
+                            part.dispose()
+                        }
+                    }
+                }
+            }
+            post("/challenge/submit"){
+                throw UnsupportedOperationException("This method is not implemented yet")
+            }
             get("/sayHello/{text}") {
                 // a ?: b  ->  if (a == null) b else a (java: (a == null) ? b : a )
                 val text = call.parameters["text"] ?: ""
@@ -76,9 +113,9 @@ fun main(args: Array<String>) {
     server.start(wait = true)
 }
 //Remove solution from challenge
-data class ChallengeWrapper(val question:String, val choices: List<String>)
+data class ChallengeWrapper(val id: Int, val question:String, val choices: List<String>)
 fun MCChallenge.removeSolution(): ChallengeWrapper {
-    return ChallengeWrapper(this.question, this.answers.keys.toList())
+    return ChallengeWrapper(this.id, this.question, this.answers.keys.toList())
 }
 
 fun getChallengeSet(tags: List<String>): List<ChallengeWrapper> {
@@ -92,8 +129,12 @@ fun getChallengeSet(tags: List<String>): List<ChallengeWrapper> {
     return list
 }
 val allChallenges = listOf<Challenge>(
-        MCChallenge(answers = mapOf("3" to false, "4" to true, "5" to false), description = "", question = "What is 2 + 2", tags = listOf("Math","Addition")),
-        MCChallenge(answers = mapOf("0" to false, "18" to false, "20" to true), description = "", question = "What is 2 * 10", tags = listOf("Math", "Multiplication")),
-        MCChallenge(answers = mapOf("Babirusa" to true, "Crocodile" to false, "Camel" to true), description = "", question = "What animals are mammals", tags = listOf("Bio")),
-        MCChallenge(answers = mapOf("Beethoven" to true, "Einstein" to false, "Mozart" to true), description = "", question = "Who were great composers", tags = listOf("Music"))
+        MCChallenge(1, answers = mapOf("3" to false, "4" to true, "5" to false), description = "", question = "What is 2 + 2", tags = listOf("Math","Addition")),
+        MCChallenge(2, answers = mapOf("0" to false, "18" to false, "20" to true), description = "", question = "What is 2 * 10", tags = listOf("Math", "Multiplication")),
+        MCChallenge(3, answers = mapOf("Babirusa" to true, "Crocodile" to false, "Camel" to true), description = "", question = "What animals are mammals", tags = listOf("Bio")),
+        MCChallenge(4, answers = mapOf("Beethoven" to true, "Einstein" to false, "Mozart" to true), description = "", question = "Who were great composers", tags = listOf("Music"))
 )
+val allTags = listOf<String>(
+        "Math", "Multiplication", "Bio", "Music"
+)
+//val testSol = MCSolution()
